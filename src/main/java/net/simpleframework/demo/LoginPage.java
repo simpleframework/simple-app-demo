de82fe95f90ac01014e5a6ff15b80748091abdf6
simpleframework <cknet@126.com>
2013-11-15 11:30:56 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/demo/LoginPage.java
+++ src/main/java/net/simpleframework/demo/LoginPage.java
@@ -12,7 +12,7 @@ import net.simpleframework.mvc.template.AbstractTemplatePage;
  * Licensed under the Apache License, Version 2.0
  * 
  * @author 陈侃(cknet@126.com, 13910090885)
- *         http://code.google.com/p/simpleframework/
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 @PageMapping(url = "/login")
